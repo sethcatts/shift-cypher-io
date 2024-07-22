b660d9d950ac06da89e3e4f41b80bf1d773af762
Seth Catts <seth.h.catts@gmail.com>
2024-07-22 16:55:27 -0400

    Creating initial files

---
+++ encoder.js
@@ -0,0 +1,35 @@
+class encoder {
+    constructor(shift) {
+    }
+
+    encode(string, shift) {
+        console.log("String: " + string)
+        let encodedString = "";
+        for (let i = 0; i < string.length; i++) {
+            let code = string.charCodeAt(i);
+            code = parseInt(code) + shift;
+            let codeLen = code.length;
+            //if code is greater than 127, loop around 
+            if (code > 127) {
+                code = code - 127;
+            } else if (code < 0) {
+                code = 0 + code;
+            }
+            encodedString += " " + String.fromCharCode(code);
+        }
+
+        return encodedString.trim();
+    }
+}
+
+let enc = new encoder();
+
+console.log(enc.encode("ABC", 1));
+
+console.assert(enc.encode("A", 1) == "B", "Did not return correct char for args A and 1");
+console.assert(enc.encode("B", -1) == "A", "Did not return correct char for args B and -1");
+
+//Tests
+/**
+ * 
+ */
